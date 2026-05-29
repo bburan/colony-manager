98795155655f790d53c927f0aee959fd10634966
Brad Buran <buran@ohsu.edu>
2026-05-28 19:08:07 -0700

    Enable debug mode

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ app = create_app()
 
 if __name__ == '__main__':
     debug = os.environ.get('FLASK_DEBUG', '0').lower() in ('1', 'true', 'yes')
-    app.run(host='0.0.0.0', port=5000, debug=debug)
+    app.run(host='0.0.0.0', port=5000, debug=True)
